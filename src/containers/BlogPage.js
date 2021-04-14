f94127623e625f14796fb8e97c8dc3cf360790c7
cassymarie <cassy2205@gmail.com>
2021-04-14 13:13:59 -0600

    basicsite setup with pages and nav links

---
+++ src/containers/BlogPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BlogPage extends Component {
+    render(){
+        return(
+            <>
+                <h1>Blog Page</h1>
+            </>
+        )
+    }
+}
+
+export default BlogPage
