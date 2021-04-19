fdd2665c7d7ac66a277f40dd7b8521eee4512e40
cassymarie <cassy2205@gmail.com>
2021-04-19 11:25:59 -0600

    comment out state for moment

--- src/components/puzzle.js
+++ src/components/puzzle.js
@@ -3,12 +3,12 @@ import PuzzleTile from '../components/puzzleTile.js'
 
 function Puzzle(){
 
-    const [tiles, tileLocation] = useState([...Array(8).keys()])
+    // const [tiles, tileLocation] = useState([...Array(8).keys()])
     
         return(
             <>
             <ul className="board">
-            
+
                 <PuzzleTile />
             </ul>
             </>
